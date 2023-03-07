072caa74a16349cae4bb028d3d76149da1312090
Abel Tadesse <abeltadesse367@gmail.com>
2023-03-07 16:31:54 +0300

    acdd error handler & 404 error

--- backend/index.js
+++ backend/index.js
@@ -28,14 +28,12 @@ app.use(bodyParser.json());
 //configure body-parser ends here
 app.use(morgan("dev")); // configire morgan
 
-
 app.use('/uploads', express.static('uploads'));
 
 // define first route
 app.get("/", (req, res) => {
   res.json("Start APIs on working");
 });
-
 
 // album URI routes
 const albumRoutes = require("./api/routes/album"); 
@@ -54,4 +52,26 @@ app.use("/artist", artistRoutes)
 //API listen PORT (4000)
 app.listen(PORT, () => {
   console.log(`App is running on ${PORT}`);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = {
+    name: "NotFound",
+    message: req.originalUrl +" Routing Not Found"
+  };
+  res.status(404).json(err);
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
